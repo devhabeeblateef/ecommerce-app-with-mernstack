cb25977ee842509123de168f0aa26f72150c1996
Habeeb Lateef <77229868+devhabeeblateef@users.noreply.github.com>
2024-05-29 21:33:20 +0100

    add custom error middleware

---
+++ server/middeware/errorMiddleware.js
@@ -0,0 +1,24 @@
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+}
+
+const errorHandler = (error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = error.message
+
+    if (error.name == "Cast Error" && error.id == "ObjectId"){
+        statusCode = 404;
+        message = "Resource not Found"
+    }
+
+    res.status(statusCode).json({
+        message,
+        stack: process.env.NODE_ENV === "production" ? null : error.stack
+    });
+
+}
+
+
+export { notFound, errorHandler };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
-
+import { notFound, errorHandler } from './middeware/errorMiddleware.js';
 const app = express();
 dotenv.config();
 const port = process.env.PORT_NUMBER 
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
     res.send("Server is ready to serve");
 })
 
+app.use(notFound)
+app.use(errorHandler)
 app.use('/api/users', userRoutes);
 
 app.listen(port, (req, res) => {
